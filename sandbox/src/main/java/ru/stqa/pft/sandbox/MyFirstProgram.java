d86e0ad1ef5669c1fd0138479fcc6182269e9db2
iboris1 <boris.ivanov2@gmail.com>
2016-12-18 13:39:35 +0200

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,28 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main (String[] args){
+    hello("world");
+    hello("user");
+    hello("Boris");
 
-		System.out.println("Hello, World!");
+    double len = 5;
+    System.out.println("Square area with side "+len+" = "+area(len));
 
-	}
+    double a = 4;
+    double b = 6;
+    System.out.println("Rectangle area with sides of "+a+" and "+b+" = "+area(a,b));
+  }
+
+	public static void hello(String somebody){
+    System.out.println("Hello, " + somebody +"!");
+  }
+
+  public static double area (double l){
+	  return l * l;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
+
 }
