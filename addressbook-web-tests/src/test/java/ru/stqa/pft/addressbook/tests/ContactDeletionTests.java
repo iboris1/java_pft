15b18007b66405cb7c0c35393c5622180df9cff5
iboris1 <boris.ivanov2@gmail.com>
2017-01-29 23:34:37 +0200

    Сравнение отсортированных списков в ContactDeletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -29,7 +30,10 @@ public class ContactDeletionTests extends TestBase {
 
     before.remove(before.size() - 1);
 
-    Assert.assertEquals(before, after);
+    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(),c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before,after);
 
   }
 
