503b5ab91ff7e4f0e14ba2a1e6a1a66da8949eaa
iboris1 <boris.ivanov2@gmail.com>
2017-04-28 11:13:40 +0300

    добавлено библиотека soap

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
   private MailHelper mailHelper;
+  private SoapHelper soapHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -84,6 +85,13 @@ public class ApplicationManager {
       mailHelper = new MailHelper(this);
     }
       return mailHelper;
+  }
+
+  public SoapHelper soap(){
+    if (soapHelper == null){
+      soapHelper = new SoapHelper(this);
+    }
+    return soapHelper;
   }
 
 }
